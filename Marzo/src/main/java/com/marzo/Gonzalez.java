66bd2adecec1c1927ac50be824312855d8baa92c
IkerDAM-1 <124777924+IkerDAM-1@users.noreply.github.com>
2023-03-09 09:40:33 +0100

    Add files via upload

---
+++ Marzo/src/main/java/com/marzo/Gonzalez.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.marzo;
+
+/**
+ *
+ * @author Iker González
+ */
+public class Gonzalez {
+    // Línea 1
+    // Línea 2
+}
